b00127970f2dedc15b881a51ab4bef44faa0899b
Farida-eng <faridaabdalkhalik44@gmail.com>
2025-12-09 19:29:05 +0200

    Update models and add new fields

--- models/product_item.py
+++ models/product_item.py
@@ -3,6 +3,7 @@ from email.policy import default
 from pkg_resources import require
 
 from odoo import models,fields
+
 class ProductItem(models.Model):
     _name="product.item"
     _description=" home tools product"
@@ -12,3 +13,21 @@ class ProductItem(models.Model):
     price=fields.Float(string="price" ,required=True)
     quantity=fields.Integer(string="Available Quantity" ,default=0)
     category_id = fields.Many2one("product.category", string="Category")
+    description=fields.Text(string="Description")
+
+    category = fields.Selection([
+        ('kitchen', 'Kitchen'),
+        ('electric', 'Electric'),
+        ('decor', 'Decor'),
+        ('furniture', 'Furniture'),
+    ], string="Category", required=True)
+
+    barcode = fields.Char(string="Barcode")
+
+    cost_price = fields.Float(string="Cost Price")
+    sale_price = fields.Float(string="Sale Price")
+
+    image = fields.Image(string="Image")
+
+    vendor_id = fields.Many2one('res.partner', string="Vendor")
+
